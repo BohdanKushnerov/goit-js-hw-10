f46b8adea856b94294a5d291cf1a10560304c572
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-02-10 20:35:43 +0200

    Add styles and js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-// https://restcountries.com/v3.1/name/ukraine
-// https://restcountries.com/v2/name/ukraine?fields=name,capital,population,languages
-
-// https://restcountries.com/v2/all?fields=name,capital,currencies
-
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages - масив мов
-
 import './css/styles.css';
 import API from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -20,77 +9,80 @@ const inputRef = document.getElementById('search-box');
 const ulRef = document.querySelector('.country-list');
 const divRef = document.querySelector('.country-info');
 
-inputRef.addEventListener('input', debounce(onSearch, 300));
-// console.log(inputRef.value)
+inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
 function onSearch(e) {
-  // const counrty = e.currentTarget.value; //не робить
   const counrty = e.target.value.trim();
-  // console.log(inputRef.value)
-  if(counrty === '') {
-    resetDivMarkup()
+  if (counrty === '') {
+    resetContainerMarkup();
+    resetListMarkup();
+    return;
   }
-  // console.log(e.target.value);
+  //   {
+  //     "status": 404,
+  //     "message": "Not Found"
+  // }
   API.fetchCountries(counrty)
-  .then(result => {
-    if(result.length > 10) {
-      Notify.info("Too many matches found. Please enter a more specific name.");
-      return
-    } else if(result.length > 2 && result.length < 10) {    
-      const markup = result.reduce(
-        (acc, value) => createUl(value) + acc,
-          ""
-          );
+    .then(result => {
+      if (result.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      } else if (result.length > 2 && result.length < 10) {
+        createListMarkup(result);
+        resetContainerMarkup();
+      } else {
+        createContainerMarkup(result);
+        resetListMarkup();
+      }
+    })
+    .catch(error => {
+      console.log(error);
 
-      ulRef.innerHTML = markup;
-      resetDivMarkup()
-    } else {
-      createMarkupDiv(result);
-      resetUlMarkup()
-    }
-  })
-  // .catch(
-  //   Notify.warning("Oops, there is no country with that name");
-  // )
-  // .finally()
-  }
-
-function createUl({name, flags}) {
-  return `
-  <li>
-    <img src=${flags.svg} alt=${name} width = '50'>
-    <h2>${name.official}</h2>
-  </li>
-  `
+      Notify.failure('Oops, there is no country with that name');
+    });
 }
 
-function createMarkupDiv ([{name, flags, capital, population, languages}]) {
-  const markup = `
-  <img src=${flags.svg} alt=${name} width = '50'>
-  <h2>${name.official}</h2>
-  <p>Capital: ${capital}</p>
-  <p>Population: ${population}</p>
-  <p>Languages: ${Object.values(languages)}</p>
-  `
+function createItem({ name, flags }) {
+  return `
+  <li class="countries-item">
+    <img class="countries-icon"src=${flags.svg} alt=${name}>
+    <h2 class="countries-title">${name.official}</h2>
+  </li>
+  `;
+}
 
+function createContainerMarkup([{ name, flags, capital, population, languages }]) {
+  const markup = `
+  <img class="country-icon" src=${flags.svg} alt=${name} width = '50'>
+  <h2 class="country-title">${name.official}</h2>
+  <p class="country-capital">Capital: ${capital}</p>
+  <p class="country-population">Population: ${population}</p>
+  <p class="country-languages">Languages: ${Object.values(languages)}</p>
+  `;
+
+  updateContainerMarkup(markup);
+}
+
+function createListMarkup(result) {
+  const markup = result.reduce((acc, value) => createItem(value) + acc, '');
+
+  updateListMarkup(markup);
+}
+
+function updateContainerMarkup(markup) {
   divRef.innerHTML = markup;
 }
 
-function createMarkupUl () {
-  
+function updateListMarkup(markup) {
+  ulRef.innerHTML = markup;
 }
 
-function resetDivMarkup() {
+function resetContainerMarkup() {
   divRef.innerHTML = '';
 }
 
-function resetUlMarkup() {
+function resetListMarkup() {
   ulRef.innerHTML = '';
 }
-
-// function updateMarkup(markup) {
-//   divRef.innerHTML = markup;
-// }
-
-// function updateUl(markup) {
-//   ulRef.innerHTML = markup;
-// }
